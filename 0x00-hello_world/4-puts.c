4cefdad0d158f606133d7307de691e52d7c10da9
EddieEngine <evansaniboy1@gmail.com>
2022-06-02 18:22:15 -0700

    fixing no description for function main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  *  *  C program that prints exactly
- *   *  *  Programming is like building a multilingual puzzle
- *    *   *Return 0 (Success)
+ *  *  *  main - entry point
+ *   *  * 
+ *    *    * Retur: Always 0 (Success)
  *     **/
 int main(void)
 {
