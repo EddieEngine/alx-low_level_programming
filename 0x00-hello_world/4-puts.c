33889afd28ac36d5412cfc5a073fdae4ab13dcba
EddieEngine <evansaniboy1@gmail.com>
2022-06-02 17:57:47 -0700

    fixed errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-
-/**
- *  * * main - entry point
- *  *   *
- *  *    *Return: Always 0 (Passed)
- *      */
 int main(void){
 	puts("\'Programming is like building a multilingual puzzle")
 	return (0);
