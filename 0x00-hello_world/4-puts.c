6a472205c94c444a2d35257fb3850617ef3bfe7a
EddieEngine <evansaniboy1@gmail.com>
2022-06-02 18:18:41 -0700

    fixing trailing whitespace in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
- * C program that prints exactly
- * Programming is like building a multilingual puzzle
- * 
- */
+
+/**
+ *  *  *  C program that prints exactly
+ *   *  *  Programming is like building a multilingual puzzle
+ *    *   *Return 0 (Success)
+ *     **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
