02021b3446de6ec3f3c0cd164cbca79c3ae040b0
EddieEngine <evansaniboy1@gmail.com>
2022-06-10 18:44:33 -0700

    upload Task 0 files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h" 
+
+/**
+ * main - test function that checks if integer is positive lr negative
+ * Return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	positive_or_negative(i);
+
+	return (0);
+
+}
